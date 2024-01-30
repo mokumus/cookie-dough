8f0f7581495a198bbcc910c05508f78f5e2f8d95
mokumus <rocketpolka@gmail.com>
2024-01-31 01:10:04 +0300

    Updates

--- public/assets/index.zDMyZYgO.js
+++
@@ -1,1 +0,0 @@
-import{d as t,F as o,v as r,x as s,A as e,B as a,C as n}from"./entry.Ztt-Y0ky.js";import{V as d,a as l}from"./VCard.wQfpkjpl.js";const f=t({__name:"index",setup(c){return o(),(i,p)=>(r(),s("div",null,[e(l,{color:"secondary",height:"800px",width:"100%"},{default:a(()=>[e(d,null,{default:a(()=>[n("Title")]),_:1})]),_:1})]))}});export{f as default};
