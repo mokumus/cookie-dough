87badc5d874b00232e12f1824d2dafa69ef01d8e
mokumus <rocketpolka@gmail.com>
2024-02-10 18:31:35 +0300

    index ai improvment

--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,30 @@
 <template>
   <v-container fluid fill-height>
-    <v-defaults-provider
-      :defaults="{ VCard: { color: 'indexCard', elevation: '10', height: '150' }, VCol: { cols: '12', sm: '6' } }"
-    >
-      <v-row>
-        <v-col>
-          <v-card to="/eslestirme">
-            <v-card-title>
-              <h2 class="text-h5">Emsile (Fiil Eşleştirme)</h2>
-            </v-card-title>
-            <v-card-text>
-              <p>Arapça alıştırma</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card to="/eslestirme-siyga">
-            <v-card-title>
-              <h2 class="text-h5">Emsile (Siga)</h2>
-            </v-card-title>
-            <v-card-text>
-              <p>Arapça alıştırma</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card to="/hadis">
-            <v-card-title>
-              <h2 class="text-h5">Hadisler</h2>
-            </v-card-title>
-            <v-card-text>
-              <p>Kütüb-i Sitte</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col>
-          <v-card to="/hadis-english">
-            <v-card-title>
-              <h2 class="text-h5">İngilizce Hadis</h2>
-            </v-card-title>
-            <v-card-text>
-              <p>İmam Buhari, İmam Müslim, Ebu Davud, Tirmizi, Nesai, İbn Mace</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-defaults-provider>
+    <v-row justify="center"> <v-col v-for="card in cards" :key="card.title" cols="12" sm="6">
+        <v-card :to="card.link" :color="indexCardColor" elevation="10" height="150">
+          <v-card-title>
+            <h2 class="text-h5">{{ card.title }}</h2>
+          </v-card-title>
+          <v-card-text>{{ card.description }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
+
 <script>
 export default {
-  data: () => ({}),
-  mounted() {},
-
-  methods: {},
-}
+  data() {
+    return {
+      cards: [
+        { title: 'Emsile (Fiil Eşleştirme)', link: '/eslestirme', description: 'Arapça alıştırma' },
+        { title: 'Emsile (Siga)', link: '/eslestirme-siyga', description: 'Arapça alıştırma' },
+        { title: 'Hadisler', link: '/hadis', description: 'Kütüb-i Sitte' },
+        { title: 'İngilizce Hadis', link: '/hadis-english', description: 'İmam Buhari, İmam Müslim, Ebu Davud, Tirmizi, Nesai, İbn Mace' },
+      ],
+      indexCardColor: 'indexCard', // Assuming this color is defined in your theme
+    };
+  },
+};
 </script>
